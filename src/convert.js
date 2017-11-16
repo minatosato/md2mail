d34d87ecf241e807d683590115e9c92ab0554090
Minato SATO <sato.minato@ohsuga.is.uec.ac.jp>
2017-11-16 20:47:36 +0900

    fix bug for unordered list

--- src/convert.js
+++ src/convert.js
@@ -24,6 +24,33 @@ SOFTWARE.
 For details, see https://github.com/anydown/maildown
 */
 
+
+/*
+MIT License
+
+Copyright (c) 2017 Minato Sato
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+For details, see https://github.com/satopirka/md2mail
+*/
+
 var kaigyo = require("./kaigyo")
 var parse = require("markdown-to-ast").parse;
 var Syntax = require("markdown-to-ast").Syntax;
@@ -44,6 +71,21 @@ function nodeToString(node) {
   }
   return output
 }
+
+function nodeToUList(node, space) {
+  var output = ""
+  if (node.value) {
+    output += space + "・" + node.value + "\n"
+  }
+  if (node.children) {
+    
+    node.children.forEach((leaf) => {
+      output += nodeToUList(leaf, space + "　")
+    })
+  }
+  return output
+}
+
 
 /**
  * convert AST to plaintext
@@ -82,9 +124,12 @@ function convert(input) {
             var listtext = node.children.map(toItem).join("")
             output.push(listtext)
           } else {
-            var toItem = listitem => "　・" + nodeToString(listitem) + "\n"
-            var listtext = node.children.map(toItem).join("")
-            output.push(listtext)
+            console.log(node);
+            var ret = [];
+            nodeToUList(node, "").split("\n").forEach(element => {
+              ret.push(element.substring(2, element.length));
+            });
+            output.push(ret.join("\n"));
           }
           return VisitorOption.Skip;
 
